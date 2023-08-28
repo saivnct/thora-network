caba7566b0289fec5827c603977f55f682cf522d
giangtpu <giangtpu@gmail.com>
2023-08-28 14:42:30 +0700

    fix test

--- miner/worker_test.go
+++ miner/worker_test.go
@@ -135,7 +135,7 @@ func newTestWorkerBackend(t *testing.T, chainConfig *params.ChainConfig, engine 
 		copy(gspec.ExtraData[32:32+common.AddressLength], testBankAddress.Bytes())
 		e.Authorize(testBankAddress, func(account accounts.Account, s string, data []byte) ([]byte, error) {
 			return crypto.Sign(crypto.Keccak256(data), testBankKey)
-		}, nil)
+		})
 	case *thora.Thora:
 		gspec.ExtraData = make([]byte, 32+common.AddressLength+crypto.SignatureLength)
 		copy(gspec.ExtraData[32:32+common.AddressLength], testBankAddress.Bytes())
